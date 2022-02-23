d0fe03c7cb6314d23282f82997d402fa0a8d592c
Alexey Siretskiy <alexey.siretskiy@gmail.com>
2022-02-23 15:06:33 +0100

    linspace  instead of a int for n_hist_bins

--- forkplot3d/parameters.py
+++ forkplot3d/parameters.py
@@ -2,11 +2,14 @@
 parameters for system and simulation
 """
 
+import numpy as np
+
 l_0  = 0.7 # the "equilibrium" length of the bond between nodes of the same size bin
 
 number_of_size_bins  = 10 # the number of size bins
 
-number_of_histogram_bins = 20 # number of bins for histogram for each node
+number_of_histogram_bins = np.linspace(0,5,30) #20 # number of bins for histogram for each node
+
 
 PLOT_ROOT ='plots'
 
